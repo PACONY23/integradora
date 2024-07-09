1b741ef826824f9efe335ffd1df1eb5068aa841e
PACONY23 <20233tn191@utez.edu.mx>
2024-07-09 16:53:02 -0600

    Seagrego una funcion de suma

--- hola.cpp
+++ hola.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
 
+static int suma(int n1, int n2);
+
 int main(){
-    cout<<"Hola mundo";
+    cout<<"Hola mundo"<<" ESta es la suma: "; 
+    cout<<suma(10,20);
     return 0;
+}
+static int suma(int n1,int n2){
+return n1 + n2;
 }
