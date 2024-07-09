e9c84828d04914bfcff7978716931474a577b03a
PACONY23 <20233tn191@utez.edu.mx>
2024-07-09 17:15:08 -0600

    Se agregaron mas funciones

--- hola.cpp
+++ hola.cpp
@@ -1,13 +1,49 @@
 #include<iostream>
 using namespace std;
 
-static int suma(int n1, int n2);
-
+static float suma(float n1, float n2);
+static float resta(float n1, float n2);
+static float multipicar(float n1, float n2);
+static float division(float n1, float n2);
+static float verifica_div(float n1);
+static void menu(int numero);
+float n1,n2;
 int main(){
-    cout<<"Hola mundo"<<" ESta es la suma: "; 
-    cout<<suma(10,20);
+    
+    cout<<"Dame dos numeros"<<endl;
+    cout<<"Numero 1: ";cin>>n1;
+    cout<<"Numero 2: ";cin>>n2;
+
     return 0;
 }
-static int suma(int n1,int n2){
-return n1 + n2;
+static float suma(float n1,float n2){
+    return n1 + n2;
+}
+static float resta(float n1, float n2){
+    return n1 -n2;    
+}
+static float multipicar(float n1, float n2){
+    return n1 * n2;    
+}
+static float division(float n1, float n2){
+    return verifica_div(n1) / n2;
+}
+static float verifica_div(float n1){
+    while (n1 < 0)
+    {
+        cout<<"Dame otro numero, np puedo dividir entre 0"<<endl;
+        cout<<"Nuevo numero: ";cin>>n1;
+    }
+    return n1;
+}
+static void menu(int numero){
+    switch (numero)
+    {
+    case 1:
+        suma(n1 , n2);
+        break;
+    
+    default:
+        break;
+    }
 }
